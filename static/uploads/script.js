37c7160dc286be037b2ac350b5240eb844cb696a
Anushka <75663460+Anushka-bot@users.noreply.github.com>
2022-05-27 02:10:54 +0530

    del

--- static/uploads/script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop()>180) {
-        $('.navbar').addClass('nav-bg');
-        console.log(`OK`);
-    } else {
-        $('.navbar').removeClass('nav-bg');
-    }
-});
